dd2a25c1fb226936f1f9401786ed3ed477c217fa
Виктор Лобынцев <sibfight@mail.ru>
2025-08-22 20:37:47 +0300

    checker_function

---
+++ checkers/post_v1_account.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+from hamcrest import assert_that, all_of, starts_with, has_property, instance_of, has_properties, equal_to
+
+
+class PostV1Account:
+
+    @classmethod
+    def check_response_values(cls, prepare_user, response):
+        today = datetime.now().strftime('%Y-%m-%d')
+        
+        assert_that(str(response.resource.registration), starts_with(today))
+
+        assert_that(
+            response, all_of(
+                has_property('resource', has_property('login', starts_with(prepare_user.login))),
+                has_property('resource', has_property('registration', instance_of(datetime))),
+                has_property(
+                    'resource', has_properties(
+                        'rating', has_properties(
+                            {
+                                "enabled": equal_to(True),
+                                "quality": equal_to(0),
+                                "quantity": equal_to(0)
+                            }
+                        )
+                    )
+                ),
+            )
+        )
--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -2,9 +2,9 @@ from datetime import datetime
 
 import pytest
 from faker import Faker
-from hamcrest import assert_that, has_property, starts_with, all_of, instance_of, has_properties, equal_to
 
 from checkers.http_checkers import check_status_code_http
+from checkers.post_v1_account import PostV1Account
 
 faker = Faker()
 now = datetime.now()
@@ -13,26 +13,13 @@ data = now.strftime("%d_%m_%Y_%H_%M_%S")
 
 def test_post_v1_account(account_helper, prepare_user):
     account_helper.register_new_user(login=prepare_user.login, password=prepare_user.password, email=prepare_user.email)
-    response = account_helper.user_login(login=prepare_user.login, password=prepare_user.password,
-                                         validate_response=True)
-
-    assert_that(
-        response, all_of(
-            has_property('resource', has_property('login', starts_with(prepare_user.login))),
-            has_property('resource', has_property('registration', instance_of(datetime))),
-            has_property(
-                'resource', has_properties(
-                    'rating', has_properties(
-                        {
-                            "enabled": equal_to(True),
-                            "quality": equal_to(0),
-                            "quantity": equal_to(0)
-                        }
-                    )
-                )
-            ),
-        )
+    response = account_helper.user_login(
+        login=prepare_user.login,
+        password=prepare_user.password,
+        validate_response=True
     )
+    
+    PostV1Account.check_response_values(prepare_user=prepare_user, response=response)
 
 
 @pytest.mark.parametrize(
